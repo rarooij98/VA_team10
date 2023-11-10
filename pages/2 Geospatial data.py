4264886f5b5fb2858aa61e5b886aab880616ea88
Robin van Rooij <robinvanrooij@live.nl>
2023-11-10 10:06:17 +0100

    show dataframes with maps

--- pages/2 Geospatial data.py
+++ pages/2 Geospatial data.py
@@ -48,6 +48,8 @@ df_map = df_map.rename(columns={'CO2 emission (Tons)': 'Emissions'})
 
 #* Map for CO2
 def co2_map(gdf, df_map):
+    # Show dataframe
+    st.write(df_map.head(5))
     # Filter gdf to include only countries in df_map
     gdf = gdf[gdf['Country'].isin(df_map['Country'])]
     # Create the map
@@ -71,10 +73,61 @@ def co2_map(gdf, df_map):
     # Display the map
     st.title('CO2 Emissions Choropleth Map')
     folium_static(m)
+    
+# def super_map(df_2):
+#     ddf = gpd.GeoDataFrame(
+#         df_2, geometry=gpd.points_from_xy(df['longitude'], df['latitude']), crs="EPSG:4326"
+#     )
+#     df_2['latitude'] = df_2['latitude']
+#     df_2['longitude'] = df_2['longitude']
+#     df_2 = df_2.dropna(subset=['latitude', 'longitude'])
+#     st.subheader('DataFrame for the Map')
+#     st.dataframe(df_2.head(3))
+#     # Check the number of rows and unique countries
+#     st.write(f"Number of Rows: {df_2.shape[0]}")
+#     st.write(f"Number of Unique Countries: {len(df_2['Country'].unique())}")
+    
+#     # Create the map
+#     m_super = folium.Map(location=[0, 0], zoom_start=2)
+#     # Add choropleth layer
+#     folium.Choropleth(
+#         geo_data=ddf,
+#         name="choropleth",
+#         data=df_2,
+#         columns=["Country", "GDP mean", 'GDP median', 'CO2 emission (Tons) mean', 'CO2 emission (Tons) median'],
+#         key_on="feature.properties.Country",
+#         fill_color="YlOrRd",
+#         fill_opacity=0.7,
+#         line_opacity=0.1,
+#     ).add_to(m_super)
+    
+#     # Add markers with pop-up information
+#     #for index, row in df_3.iterrows():
+#     folium.Marker(
+#         location=[row['latitude'], row['longitude']],
+#         popup=f"Country: {row['Country']}<br>GDP: {row['GDP']}<br>Population: {row['Population(2022)']}<br>Area: {row['Area']}<br>CO2 Emission: {row['CO2 emission (Tons)']}",
+#         icon=folium.Icon(color='blue')
+#     ).add_to(m_super)
+#     # Add markers with pop-up information
+#     for index, row in df_2.iterrows():
+#         folium.Marker(
+#             location=[row['latitude'], row['longitude']],
+#             popup=("Country: {0}<br>GDP mean: {1}<br>GDP median: {2}"
+#                    "<br>CO2 emission (Tons) mean: {3}<br>CO2 emission (Tons) median: {4}"
+#                    .format(row['Country'], row['GDP mean'], row['GDP median'],
+#                            row['CO2 emission (Tons) mean'], row['CO2 emission (Tons) median'])),
+#             icon=folium.Icon(color='blue')
+#         ).add_to(m_super)
+    
+#     # Display the map
+#     st.title('GDP and Emission mena and median Map')
+#     folium_static(m_super)
 
 #* Map for GDP
 def gdp_map(gdf):
+        # Show dataframe
         df_map_GDP = df_2[['Country', 'Year', 'GDP']]
+        st.write(df_map_GDP.head(5))
         # Filter gdf to include only countries in df_map
         gdf = gdf[gdf['Country'].isin(df_map_GDP['Country'])]
         # Choose Year
@@ -122,11 +175,13 @@ def get_mean(df_2):
         contact_option_3 = df_3['Country']
         contact_selected_3 = st.selectbox("Kies een jaar", options=contact_option_3, key="mean")
         df_3[df_3['Country']==contact_selected_3]
+        return df_3
 
 #* Tabs with content
 tab1, tab2, tab3 = st.tabs(["CO2 Map", "GDP Map", "Gemiddeldes per land"])
 with tab1:
     co2_map(gdf, df_map)
+    #super_map(df_2)
 with tab2:
     gdp_map(gdf)
 with tab3:
